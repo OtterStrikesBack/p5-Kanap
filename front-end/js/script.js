9a2163a01f45de0ddcd69660b309eb79151a989a
OtterStrikesBack <ju_morandin@hotmail.fr>
2022-08-04 12:07:36 +0200

    refactoring functions

--- front-end/js/script.js
+++ front-end/js/script.js
@@ -1,22 +1,47 @@
 fetch("http://localhost:3000/api/products")
 .then((res) => res.json())
-.then((data) =>  addProduct(data))
+.then((data) =>  {
+    console.log(data)
+    return addProduct(data)
+})
 
 
-function addProduct(data) {
+function addProduct(data) {    
+    const id = data[0]._id
+    const anchor = createAnchor(id)
+    appendChildren(anchor)
+}
 
-    
-    const imageUrl = data[0].imageUrl
-
-
+function createAnchor(id){
     const anchor = document.createElement("a")
-    anchor.href = imageUrl
-    anchor.text = "Kanap Sinopé"
+    anchor.href = "./product.html?id=42" + id
+    return anchor
+}
 
+function appendChildren(anchor) {
     const items = document.querySelector("#items")
     if (items) {
         items.appendChild(anchor)
     }
+
 }
 
+function createArticle() {
+
+}
+
+function createImage() {
+
+
+}
+
+function createH3() {
+
+
+}
+
+function createParagraph() {
+
+
+}
 
